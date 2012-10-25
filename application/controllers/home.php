1736ec44609004e015213e0d82a125727c358c52
Sachiano <sacha.corazzi@gmail.com>
2012-10-25 20:29:29 +0100

    Initial Commit

---
+++ application/controllers/home.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->helper('url');
+		
+		$this->load->view('head');
+		$this->load->view('home');
+		$this->load->view('footer');
+	}
+}
+
+/* End of file home.php */
+/* Location: ./application/controllers/home.php */
