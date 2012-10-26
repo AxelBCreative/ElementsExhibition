a7fefd7c8e4aa0b53e42bc1d3b48dac82bb01d78
Sachiano <sacha.corazzi@gmail.com>
2012-10-26 13:57:49 +0100

    Partial Success!
    Nesting a new row with the image inside the parent row of the body copy
    gets them to line up. Not exactly what I wanted, but close enough for a
    commit!

--- application/views/foundation/home.php
+++ application/views/foundation/home.php
@@ -1,14 +1,19 @@
 	<div class="row">
+	
 			<div class="five columns">
 				<?php echo $heading; ?>
 			</div>
-			<div class="five columns">
-				<a href="vip/invite"><img src="<?php echo $inviteimage; ?>" /></a>
+			<div class="row">
+				
+				<div class="seven columns">
+					<?php echo $copy; ?>
+				</div>
+				
+				<div class="row">
+					<div class="five columns">
+						<a href="vip/invite"><img src="<?php echo $inviteimage; ?>" /></a>
+					</div>
+				</div>	
+				
 			</div>
-	</div>		
-		
-	<div class="row">
-		<div class="seven columns">
-			<?php echo $copy; ?>
-		</div>
-	</div>
+	</div><!--.row-->
